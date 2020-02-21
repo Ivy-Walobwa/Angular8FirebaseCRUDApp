ee35aa7fd15c6a1bdc2257391efdcf56e7a8f73d
Ivy Walobwa <ivywalobwa@gmail.com>
2020-02-21 16:41:20 +0300

    file update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyCP3CzDVbRn3lVvpYlsWinkfzkj2uOL-Is',
-    authDomain: 'angularproject-3d3b7.firebaseapp.com',
-    databaseURL: 'https://angularproject-3d3b7.firebaseio.com',
-    projectId: 'angularproject-3d3b7',
-    storageBucket: 'angularproject-3d3b7.appspot.com',
-    messagingSenderId: '289728393184',
-    appId: '1:289728393184:web:abdeb41890fa19e3624585',
-    measurementId: 'G-9H9WPD19RC'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: ''
   }
 };
 
